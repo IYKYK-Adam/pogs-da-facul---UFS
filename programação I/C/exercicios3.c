de98a4b8334fffdbfeef7118152259c18b7397bd
Adam <111666668+IYKYK-Adam@users.noreply.github.com>
2026-04-12 18:47:34 -0300

    📝 estudando...

---
+++ programação I/C/exercicios3.c
@@ -0,0 +1,151 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    //rdiciação
+    
+    /*double x, raiz;
+    printf("Informe um numero: ");
+    scanf("%lf", &x);
+    
+    raiz = sqrt(x);
+    
+    printf("A raiz do número %.2lf eh: %.2lf \n", x, raiz);
+    
+    return 0;
+    
+    
+    //potenciação
+    
+    double x, y, pot;
+    
+    printf("Insira um numero: ");
+    scanf("%lf", &x);
+    
+    printf("informe o valor do expoente: ");
+    scanf("%lf", &y);
+    
+    pot = pow(x, y);
+    
+    printf("o numero %.2lf elevado a %.2lf eh: %.2lf \n", x, y, pot);
+    
+    return 0;
+    
+    
+    //arredendar
+    
+    double n, n_floor, n_ceil, n_round;
+    
+    printf("informe um numero decimal: ");
+    scanf("%lf", &n);
+    
+    n_floor = floor(n);
+    n_ceil = ceil(n);
+    n_round = round(n);
+    
+    printf("Saida a funcao floor: %.2lf.\n", n_floor);
+    printf("Sainda da funcao ceil: %.2lf.\n", n_ceil);
+    printf("Saida da funcao round: %.2lf.\n", n_round);
+    
+    return 0;
+    
+    //EXERCICIOS//
+    
+    //1
+    
+    double n, quad, cubo;
+    
+    printf("digite um numero: ");
+    scanf("%lf", &n);
+    
+    quad = pow(n, 2);
+    cubo = pow(n, 3);
+    
+    printf("%.2lf elevado ao quadrado eh: %.2lf. \n%.2lf elevado ao cubo eh: %.2lf", n, quad, n, cubo);
+    
+    return 0;
+    
+    //2 
+    
+    double n1, n2, raiz;
+    
+    printf("digite dois numeros: ");
+    scanf("%lf %lf", &n1, &n2);
+    
+    raiz = sqrt(n1 * n2);
+    
+    printf("a raiz da multiplicação desses dois numeros eh: %.2lf \n", raiz);
+    
+    return 0;
+    
+    //3 
+    
+    double n1, razao, n, PA;
+    
+    printf(" | CALCULADORA DE PA | \n");
+    
+    printf("digite o primeiro termo da sequencia: ");
+    scanf("%lf", &n1);
+    
+    printf("digite a razao da expressao: ");
+    scanf("%lf", &razao);
+    
+    printf("digite o valor de n: ");
+    scanf("%lf", &n);
+    
+    PA = n1 + (n-1) * razao;
+    
+    printf("o resultado da razão eh: %.2lf \n", PA);
+    
+    return 0;*/
+    
+    /*4  Construa um algoritmo em C que, tendo como dados de entrada dois pontos
+    quaisquer no plano, P(x1,y1) e P(x2,y2) informados pelo usuário, calcule a
+    distância entre eles.
+
+    double x1, x2, y1, y2, d;
+
+    printf("| PRIMEIRO PONTO | \n");
+
+    printf("digite o X do primeiro ponto: ");
+    scanf("%lf", &x1);
+    printf("digite o Y do primeiro ponto: ");
+    scanf("%lf", &y1);
+
+    printf("| SEGUNDO PONTO | \n");
+
+    printf("digite o X do segundo ponto: ");
+    scanf("%lf", &x2);
+    printf("digite o Y do segundo ponto: ");
+    scanf("%lf", &y2);
+
+    d = sqrt( pow(x1 - x2, 2) +  pow(y1 - y2, 2) );
+
+    printf("A distancia entre P(%.2lf,%.2lf) e P(%.2lf,%.2lf) eh: %.2lf \n", x1, y1, x2, y2, d);
+
+    return 0;
+    */
+
+    /*5. Escreva um algoritmo em C que recebe três números inteiros e posiBvos (A,B, C) informados pelo usuário e calcula a seguinte expressão:
+
+    double A, B, C;
+
+    printf("Digite o valor de A: ");
+    scanf("%lf", &A);
+
+    printf("Digite o valor de B: ");
+    scanf("%lf", &B);
+
+    printf("Digite o valor de C: ");
+    scanf("%lf", &C);
+
+    printf("|EXPRESSAO R = (A + B)2| \n");
+    printf("O RESULTADO EH: %.2lf \n", pow(A + B, 2));
+
+    printf("|EXPRESSAO S = (B + C)2| \n");
+    printf("O RESULTADO EH: %.2lf \n", pow(B + C, 2));
+
+    return 0;
+    */
+} 
